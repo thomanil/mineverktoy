abff101d4af5c66928892b7571ab65c76bf1ea05
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-03-06 22:06:50 +0100

    interview route back in

--- mineverktoy.rb
+++ mineverktoy.rb
@@ -4,11 +4,10 @@ get '/' do
   erb :index
 end
 
-#get '/om' do
- # erb :about
-#end
+get '/om' do
+  erb :about
+end
 
-#get '/:name' do |name|
- # erb :"#{name}"
-    # would instead render views/dogs/index.erb
-#end
+get '/:name' do |name|
+  erb :"#{name}"
+end
