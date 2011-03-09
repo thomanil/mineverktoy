8c0040b927af88b9137cc09a9ed56b8e244503df
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-03-09 22:45:49 +0100

    Creating headers, pictures, index based on the interview metadata now

--- interviews.rb
+++ interviews.rb
@@ -1,6 +1,6 @@
 def interviews_config
-  [ {:published_at => 1299704014, :name => "john-smith", :title => "Chief King" }, 
-    {:published_at => 1299904200, :name => "jane-doe"  , :title => "Head Queen"} ]
+  [ {:published_timestamp => 1299704014, :published_at => "21.03.2011", :name => "john-smith", :full_name => "John Smith The First", :title => "Chief King" }, 
+    {:published_timestamp => 1299904200, :published_at => "02.06.2011", :name => "jane-doe" , :full_name => "Jane Doe", :title => "Head Queen"} ]
 end
 
 # Converting to/from timestamp
--- mineverktoy.rb
+++ mineverktoy.rb
@@ -7,8 +7,11 @@ helpers do
   def interview_list  
     list = "";
     interviews_config().each do |interview| 
+      name = interview[:name]
+      
       list += "<p class='summary'>"
-      list += "<a href='#{interview[:name]}'>#{interview[:name]}</a>"
+      list += "<img class='thumbnail' src='images/#{name}/thumbnail-bw.jpg'><img class='hidden thumbnail' src='images/#{name}/thumbnail-color.jpg'>"
+      list += "<a href='#{name}'>#{name}</a>"
       list += "</p>"
     end
     return list
@@ -22,6 +25,43 @@ helpers do
     "<a href='om'>Om nettstedet</a>"
   end
   
+  def q1_who_are_you?
+    "<h2>Hvem er du, og hva holder du på med?</h2>"
+  end
+  
+  def q2_what_hardware?
+    "<h2>Hva slags maskinvare bruker du?</h2>"
+  end
+  
+  def q3_what_software?
+    "<h2>Og hva slags programvare?</h2>"
+  end
+  
+  def q4_dream_setup?
+    "<h2>Hva er drømmeoppsettet ditt?</h2>"
+  end
+  
+  def interview_metadata name
+    interviews_config.find { |config| config[:name].eql? name }
+  end
+  
+  def portrait_link name
+    "<img src='/images/#{name}/portrait.jpg'>"
+  end
+  
+  def interview_header name
+    meta_data = interview_metadata name
+    published_at = meta_data[:published_at]
+    title = meta_data[:title]
+    full_name = meta_data[:full_name]
+
+    header = ""
+    header += "<p>#{portrait_link name}</p>"
+    header += "<h2>#{full_name}</h2>"
+    header += "<p>#{title} - #{published_at}</p>"
+    
+    return header
+  end
   
 end
 
@@ -35,6 +75,8 @@ get '/om' do
 end
 
 get '/:name' do |name|
+  @interview_header = interview_header name
+  
   # find that interview from hash-array, populate page with info taken from there
   # make parts of page generated, ie publish date etc
   # also inject questions
