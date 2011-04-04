4628470eb4979f092346962fac2d172119e1844a
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-04-04 19:21:24 +0200

    Linkiefied and added laptop img to espens interview

--- interviews.rb
+++ interviews.rb
@@ -5,8 +5,8 @@ end
 def interviews_config
   [{:published? => true, :published_timestamp => 1301334838, :published_at => '28.03.2011', :name => 'andre-eide', :full_name => 'André Eide', :title => 'Selvstendig næringsdrivende og fotograf'},
    {:published? => true, :published_timestamp => 1301855769, :published_at => '04.04 2011', :name => 'eli-toftoy-andersen', :full_name => 'Eli Toftøy-Andersen', :title => 'Brukervennlighetsrådgiver hos Steria'},    
+   {:published? => false, :published_timestamp => 1301146613, :published_at => 'IKKE PUBLISERT ENNÅ', :name => 'espen-dallokken', :full_name => 'Espen Dalløkken', :title => 'Framsiearkitekt hos FINN.no'},
    {:published? => false, :published_timestamp => 1300391014, :published_at => 'IKKE PUBLISERT ENNÅ', :name => 'johannes-brodwall', :full_name => 'Johannes Brodwall', :title => 'Chief Scientist hos Steria'},
    {:published? => false, :published_timestamp => 1301145749, :published_at => 'IKKE PUBLISERT ENNÅ', :name => 'christian-johansen', :full_name => 'Christian Johansen', :title => 'Programmerer hos Gitorious'},
-   {:published? => false, :published_timestamp => 1301146613, :published_at => 'IKKE PUBLISERT ENNÅ', :name => 'espen-dallokken', :full_name => 'Espen Dalløkken', :title => 'Framsiearkitekt hos FINN.no'},
    {:published? => false, :published_timestamp => 1301147703, :published_at => 'IKKE PUBLISERT ENNÅ', :name => 'ole-morten-amundsen', :full_name => 'Ole Morten Amundsen', :title => 'Entreprenør og programmerer'}]
 end
