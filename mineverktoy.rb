c323754abfaf777447a3d554b751156555de1364
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-05-01 21:09:00 +0200

    Stating timezone name in rss

--- mineverktoy.rb
+++ mineverktoy.rb
@@ -104,7 +104,7 @@ helpers do
             xml.item do
               xml.title "Hvilke verktøy bruker #{interview[:full_name]}?"
               xml.link "http://mineverktoy.com/#{interview[:name]}"
-              xml.pubDate Time.at(interview[:published_timestamp]).strftime("%a, %d %b %Y %k:%M:%S %z")          
+              xml.pubDate Time.at(interview[:published_timestamp]).strftime("%a, %d %b %Y %k:%M:%S %Z")          
            
             end
           end
