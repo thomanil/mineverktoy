cfffcde33c6b45e2169535e8fa7b91dd25a9f62c
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-05-30 09:39:15 +0200

    Linkified and published Torbjørns interview

--- interviews.rb
+++ interviews.rb
@@ -12,5 +12,5 @@ def interviews_config
    {:published? => true, :published_timestamp => 1304921718, :published_at => '09.05 2011', :name => 'espen-antonsen', :full_name => 'Espen Antonsen', :title => 'Utvikler'},
    {:published? => true, :published_timestamp => 1305529393, :published_at => '16.05 2011', :name => 'thomas-flemming', :full_name => 'Thomas Flemming', :title => 'Programmerer'},
    {:published? => true, :published_timestamp => 1306147243, :published_at => '23.05.2011', :name => 'trygve-lie', :full_name => 'Trygve Lie', :title => 'Framside-utvikler hos A-pressen Digitale Medier'},
-   {:published? => false, :published_timestamp => 1305570267, :published_at => 'Ikke publisert', :name => 'torbjorn-maro', :full_name => 'Torbjørn Marø', :title => 'Senior utvikler og teamleder hos PSWinCom'}]
+   {:published? => true, :published_timestamp => 1306740066, :published_at => '30.05.2011', :name => 'torbjorn-maro', :full_name => 'Torbjørn Marø', :title => 'Senior utvikler og teamleder hos PSWinCom'}]
 end
