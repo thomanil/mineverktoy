05b6f5bcb56bb43962623c0bfc911fa71ea20ab2
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-08-21 09:31:06 +0200

    Added Espens Andersen interview

--- interviews.rb
+++ interviews.rb
@@ -22,5 +22,6 @@ def interviews_config
    {:published? => false, :published_timestamp => 1311073211, :published_at => '19.07 2011', :name => 'karianne-berg', :full_name => 'Karianne Berg', :title => 'Utvikler og konsulent hos Miles'},
    {:published? => false, :published_timestamp => 1313428271, :published_at => '15.08 2011', :name => 'mia-holte', :full_name => 'Mia Holte', :title => 'Frilans webdesigner'},
    {:published? => false, :published_timestamp => 1313428333, :published_at => '15.08 2011', :name => 'jon-bing', :full_name => 'Jon Bing', :title => 'Professor hos det juridiske fakultet ved UiO'},
-   {:published? => false, :published_timestamp => 1313504607, :published_at => '16.08 2011', :name => 'odin-hole-standal', :full_name => 'Odin Hole Standal', :title => 'Konsulent hos Webstep'}]
+   {:published? => false, :published_timestamp => 1313504607, :published_at => '16.08 2011', :name => 'odin-hole-standal', :full_name => 'Odin Hole Standal', :title => 'Konsulent hos Webstep'},
+   {:published? => false, :published_timestamp => 1313910886, :published_at => '21.08 2011', :name => 'espen-andersen', :full_name => 'Espen Andersen', :title => 'Foreleser, forsker og skribent'}]
 end
