b9fde08128edb5595c2a39c3642443bb1a33a6eb
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-08-29 06:27:36 +0200

    Published Kariannes interview

--- interviews.rb
+++ interviews.rb
@@ -21,7 +21,7 @@ def interviews_config
    {:published? => true, :published_timestamp => 1311073275, :published_at => '08.08 2011', :name => 'sarah-brodwall', :full_name => 'Sarah Brodwall', :title => 'Webdesigner'},
    {:published? => true, :published_timestamp => 1313382367, :published_at => '15.08.2011', :name => 'rolf-rander-ness', :full_name => 'Rolf Rander Næss', :title => 'Rådgiver hos Acando'},
    {:published? => true, :published_timestamp => 1313987104, :published_at => '22.08 2011', :name => 'espen-andersen', :full_name => 'Espen Andersen', :title => 'Foreleser, forsker og skribent'},
-   {:published? => false, :published_timestamp => 1311073211, :published_at => '19.07 2011', :name => 'karianne-berg', :full_name => 'Karianne Berg', :title => 'Utvikler og konsulent hos Miles'},
+   {:published? => true, :published_timestamp => 1314591941, :published_at => '29.07 2011', :name => 'karianne-berg', :full_name => 'Karianne Berg', :title => 'Utvikler og konsulent hos Miles'},
    {:published? => false, :published_timestamp => 1313428271, :published_at => '15.08 2011', :name => 'mia-holte', :full_name => 'Mia Holte', :title => 'Frilans webdesigner'},
    {:published? => false, :published_timestamp => 1313428333, :published_at => '15.08 2011', :name => 'jon-bing', :full_name => 'Jon Bing', :title => 'Professor hos det juridiske fakultet ved UiO'},
    {:published? => false, :published_timestamp => 1313504607, :published_at => '16.08 2011', :name => 'paul-chaffey', :full_name => 'Paul Chaffey', :title => 'Adm dir i Abelia'},
