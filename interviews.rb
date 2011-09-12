15ddf6f7fc591df5663653c3a0af889f4bdc6b03
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-09-12 06:28:35 +0200

    Published Mortens interview

--- interviews.rb
+++ interviews.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def published_interviews_config
   interviews_config.select { |interview| interview[:published?] }
 end
@@ -23,8 +24,8 @@ def interviews_config
    {:published? => true, :published_timestamp => 1313987104, :published_at => '22.08.2011', :name => 'espen-andersen', :full_name => 'Espen Andersen', :title => 'Foreleser, forsker og skribent'},
    {:published? => true, :published_timestamp => 1314591941, :published_at => '29.07.2011', :name => 'karianne-berg', :full_name => 'Karianne Berg', :title => 'Utvikler og konsulent hos Miles'},
    {:published? => true, :published_timestamp => 1315196668, :published_at => '05.09.2011', :name => 'mia-holte', :full_name => 'Mia Holte', :title => 'Frilans webdesigner'},
+   {:published? => true, :published_timestamp => 1315801609, :published_at => '12.09 2011', :name => 'morten-tollefsen', :full_name => 'Morten Tollefsen', :title => 'Forskningsleder i MediaLT'},
    {:published? => false, :published_timestamp => 1313428333, :published_at => '15.08.2011', :name => 'jon-bing', :full_name => 'Jon Bing', :title => 'Professor hos det juridiske fakultet ved UiO'},
    {:published? => false, :published_timestamp => 1313504607, :published_at => '16.08.2011', :name => 'paul-chaffey', :full_name => 'Paul Chaffey', :title => 'Adm dir i Abelia'},
-   {:published? => false, :published_timestamp => 1313504607, :published_at => '16.08.2011', :name => 'joachim-froholt', :full_name => 'Joachim Froholt', :title => 'Skribent hos Gamer.no'},
-   {:published? => false, :published_timestamp => 1315245245, :published_at => '05.09 2011', :name => 'morten-tollefsen', :full_name => 'Morten Tollefsen', :title => 'Forskningsleder i MediaLT'}]
+   {:published? => false, :published_timestamp => 1313504607, :published_at => '16.08.2011', :name => 'joachim-froholt', :full_name => 'Joachim Froholt', :title => 'Skribent hos Gamer.no'}]
 end
