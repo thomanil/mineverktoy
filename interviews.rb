a2e7e36b3396c390ed97d0ea00840c3ed23fb5a8
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-11-09 00:34:59 +0100

    Tweaks

--- interviews.rb
+++ interviews.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 def published_interviews_config
   interviews_config.select { |interview| interview[:published?] }
 end
--- mineverktoy.rb
+++ mineverktoy.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+require 'rubygems'
 require 'sinatra'
-require 'interviews'
+require './interviews'
 
 get '/' do
   erb :index
