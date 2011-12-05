331b9b88dc5b7acae3e7c251166f6e22c8b5f3ac
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-12-05 21:42:46 +0100

    Published Rams interview

--- interviews.rb
+++ interviews.rb
@@ -36,6 +36,6 @@ def interviews_config
    {:published? => true, :published_timestamp => 1320690180, :published_at => '07.11 2011', :name => 'ole-christian-storesund', :full_name => 'Ole Christian Storesund', :title => 'Grafisk designer'},
    {:published? => true, :published_timestamp => 1321271287, :published_at => '14.11 2011', :name => 'mats-lindh', :full_name => 'Mats Lindh', :title => 'Utvikler, innvikler og hacker'},
    {:published? => false, :published_timestamp => 1321296887, :published_at => '14.11 2011', :name => 'sondre-bjellas', :full_name => 'Sondre Bjellås', :title => 'Senior løsningsarkitekt hos Deepmind AS'},
-   {:published? => false, :published_timestamp => 1322475091, :published_at => '28.11 2011', :name => 'ram-yoga', :full_name => 'Ram Yoga', :title => 'Interaksjonsdesigner og brukervennlighetsrådgiver'},
+   {:published? => true, :published_timestamp => 1323117735, :published_at => '05.12 2011', :name => 'ram-yoga', :full_name => 'Ram Yoga', :title => 'Interaksjonsdesigner og brukervennlighetsrådgiver'},
    {:published? => true, :published_timestamp => 1322477426, :published_at => '28.11 2011', :name => 'rune-spaans', :full_name => 'Rune Spaans', :title => 'Uavhengig illustratør og regissør'}]
 end
