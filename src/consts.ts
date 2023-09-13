12a1dc571ee23eadca64c8e4e582c8bbacb47e70
Thomas Kjeldahl Nilsson <thomanil@monio.no>
2023-09-13 20:28:18 +0200

    Move over portraits and tweak styles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mine Verktøy';
+export const SITE_DESCRIPTION = 'Hva bruker folk for å få ting gjort?';
