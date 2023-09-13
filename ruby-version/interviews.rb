eb10ad591b6850baff060bf8a4b77007e4333321
Thomas Kjeldahl Nilsson <thomanil@monio.no>
2023-09-13 23:44:15 +0200

    Flemming and Marø migrated

--- ruby-version/interviews.rb
+++ ruby-version/interviews.rb
@@ -8,7 +8,5 @@ def interviews_config
   [
 
 
-   {:published? => true, :published_timestamp => 1305529393, :published_at => '16.05.2011', :name => 'thomas-flemming', :full_name => 'Thomas Flemming', :title => 'Programmerer'},
    {:published? => true, :published_timestamp => 1306147243, :published_at => '23.05.2011', :name => 'trygve-lie', :full_name => 'Trygve Lie', :title => 'Framside-utvikler hos A-pressen Digitale Medier'},
-   {:published? => true, :published_timestamp => 1306740066, :published_at => '30.05.2011', :name => 'torbjorn-maro', :full_name => 'Torbjørn Marø', :title => 'Senior utvikler og teamleder hos PSWinCom'},
 end
