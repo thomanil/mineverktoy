d35b7029ec9ed1400f85f4effb62dc3355fc94c1
Thomas Kjeldahl Nilsson <thomanil@monio.no>
2023-09-14 00:53:14 +0200

    Change urls back to not include base again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	//site: 'https://mineverktoy.com',
-	site: 'https://thomanil.github.io',
-	base: '/mineverktoy', // Remove once I set custom domain
+	site: 'https://mineverktoy.com',
+	//site: 'https://thomanil.github.io',
+	//base: '/mineverktoy', // Only have this in if no custom domain in place
 	integrations: [mdx(), sitemap()],
 });
