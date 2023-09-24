7022d66dcd2793ae9bcf5eef1dd52c4d8bcc6c5e
Thomas Kjeldahl Nilsson <thomanil@monio.no>
2023-09-24 18:58:54 +0200

    Add draft flag so index does not expose those until I'm ready

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		draft: z.boolean().optional(),
 	}),
 });
 
