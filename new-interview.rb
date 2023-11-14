e831671d07402f17e206994b1e2968d38a4207f2
Thomas Kjeldahl Nilsson <thomanil@monio.no>
2023-11-14 10:25:02 +0100

    More on script

--- new-interview.rb
+++ new-interview.rb
@@ -1,20 +1,29 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+include FileUtils
+
 
 puts "Hva er navnet til personen?"
-name = gets
+name = gets.chomp
 
 puts "Hva er tittelen til personen?"
-title = gets
+title = gets.chomp
 
 puts "Hva skal være path ('fornavn-etternavn') til intervju, bilder etc?"
-path = gets
+path = gets.chomp
 
-puts "\nGenerating..."
 
-mkdir ".\public\images\$path"
+imageDirPath = "./public/images/#{path}"
+mdxPath = "./src/content/blog/#{path}.mdx"
 
 # TODO Create image folder
 # TODO Copy template mdx file, with substitions
+
+mkdir_p imageDirPath
+mkdir_p mdxPath
+
+puts "\nGenerated interview mdx file: #{mdxPath}"
+puts "\nGenerated img directory: #{imageDirPath}"
 
 
